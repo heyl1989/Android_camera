52de57b2bc460f01f2a15231457381a952eb8fce
Administrator <heyongliang>
2019-10-15 22:04:11 +0800

    Initial commit

---
+++ app/src/main/java/heyl/camera/android/android_camera/MainActivity.java
@@ -0,0 +1,29 @@
+package heyl.camera.android.android_camera;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }
+        });
+    }
+}
