669c888c8f38cc887e31c55909b10a386722d347
Yongliang He <heyl2804@gmail.com>
2019-10-24 10:18:54 +0800

    跳转相机拍照并回显

--- camera/src/main/java/heyl/camera/android/camera/MainActivity.java
+++ camera/src/main/java/heyl/camera/android/camera/MainActivity.java
@@ -1,18 +1,49 @@
 package heyl.camera.android.camera;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int REQ_1 = 0x01;
+    private final int REQ_2 = 0x02;
+    private ImageView mImageview;
+    private String filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
         initView();
+    }
+
+    private void initData() {
+        filePath = Environment.getExternalStorageDirectory().getPath();
+        filePath = filePath + "/" + "temp.png";
     }
 
     private void initView() {
@@ -20,8 +51,82 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(intent);
+                startActivityForResult(intent, REQ_1);
             }
         });
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                applyWritePermission();
+            }
+        });
+        mImageview = findViewById(R.id.imageView);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (Activity.RESULT_OK == resultCode) {
+            if (REQ_1 == requestCode) {
+                Bundle bundle = data.getExtras();
+                Bitmap bitmap = (Bitmap) bundle.get("data");
+                mImageview.setImageBitmap(bitmap);
+            }
+            if (REQ_2 == requestCode) {
+                FileInputStream fileInputStream = null;
+                try {
+                    fileInputStream = new FileInputStream(filePath);
+                    Bitmap bitmap = BitmapFactory.decodeStream(fileInputStream);
+                    mImageview.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        fileInputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private void takePhoto(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri photoPath = FileProvider.getUriForFile(MainActivity.this,
+                getPackageName()+".fileProvider", new File(filePath));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoPath);
+        startActivityForResult(intent, REQ_2);
+    }
+    public void applyWritePermission() {
+        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int checkPermission1 = ContextCompat.checkSelfPermission(this, permissions[0]);
+            // 权限是否已经 授权 GRANTED---授权  DINIED---拒绝
+            if (checkPermission1 == PackageManager.PERMISSION_GRANTED ) {
+                takePhoto();
+            } else {
+                requestPermissions(permissions, 1);
+            }
+        } else {
+            takePhoto();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1 && grantResults.length > 0) {
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(MainActivity.this,"权限禁止",Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+            takePhoto();
+        } else {
+            Toast.makeText(MainActivity.this,"权限禁止",Toast.LENGTH_LONG).show();
+        }
     }
 }
